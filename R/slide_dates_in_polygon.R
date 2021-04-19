fbd619a5af8f4dbd33a5524d744fa1168b1e862f
RobinKohrs <robin.kohrs@gmx.de>
2021-04-19 11:10:12 +0200

    merge slide_dates_in_polygon from develop branch

--- R/slide_dates_in_polygon.R
+++ R/slide_dates_in_polygon.R
@@ -1,22 +1,35 @@
 #' Extracts the days of the slides for each polygon
 #'
+#' @description  For each Polygon this function selects only the polygons that experienced a landsld. It also reports (in the column \code{slide_per_poly_date})
+#' the number of slides on that date in that polygon.shapefile
+#'
+#' @return An object of class \code{sf} which is (probably) of geometry-type polygon with the additional columns.
+#'
 #' @importFrom dplyr group_by distinct
 #'
 #' @param poly Path to the polygon shapefile
-#' @param iffi10 Path to the iffi10 shapefile with already precomputed columns in english and the date information
+#' @param landsld Path to the landsld shapefile or object of class \code{sf}. Needs to have at least a column called \code{date_info}
+#' (as in the classic iffi-data from the \code{iffitoR}-package) or column called \code{date} with dates of class \code{date}. If both do not
+#' exist, the first column with the class \code{date} will be used to subset only the landslides with day-precise information
 #'
 #' @export
 
 slide_dates_in_polygon = function(
   poly = NULL,
-  iffi10 = "\\\\projectdata.eurac.edu/projects/Proslide/Landslides/Iffi_db_xxxx_to_2018/exportperEurac2020/Shapefiles/IFFI10_1.shp",
+  landsld = NULL,
   epsg = 32632
 ){
 
-  ####### check if the paths exist
+  ####### check if input provided
   if (is.null(poly)) {
-    stop("You need to pass the path to the polygon")
+    stop("You need to provide some polygons")
   }
+
+  if (is.null(landsld)) {
+    stop("You need to provide some landsldide dataframe")
+  }
+
+  ##### Check the inputs
 
   if (class(poly)[1] == "sf") {
     print("using an sf-object as polygons")
@@ -33,18 +46,16 @@ slide_dates_in_polygon = function(
     }
   }
 
-
-
   # check if using a path to a spatial object or an object of type sf already
-  if (is.character(iffi10)) {
+  if (is.character(landsld)) {
     c = T
-    if (!file.exists(iffi10)) {
-      stop("The IFFI10 path is not valid")
+    if (!file.exists(landsld)) {
+      stop("The landsld path is not valid")
     }
   }else{
     c = FALSE
     # if you already passed a spatial object
-    if(!class(iffi10)[1] == "sf"){
+    if(!class(landsld)[1] == "sf"){
       stop("The iffi parameter must be of class sf")
     }else{
       print("using an sf-object as input to the iffi-argument")
@@ -54,36 +65,48 @@ slide_dates_in_polygon = function(
 
   ####### load the data
   if(c_p) poly = st_read(poly)
-  if(c) iffi10 = st_read(iffi10)
+  if(c) landsld = st_read(landsld)
 
   ####### filter the iffi data to only use dates
 
   # filter only the ones that have day information
   # get the names
-  nams = names(iffi10)
+  nams = names(landsld)
 
-  if (!"date_info" %in% nams) {
-    stop(
-      "The iffi10 dataframe you are using has no column called 'date_info \nwhich is necessary for the subsetting'"
-    )
-  } else{
-    iffi10 = iffi10[iffi10$date_info == "day",]
+  if ("date_info" %in% nams) {
+    if (all(grepl("day|month|year|no date", landsld[["date_info"]]))) {
+      landsld = landsld[landsld$date_info == "day", ]
+    }
+  } else if("date" %in% nams){
+    if (all(grepl("day|month|year|no date", landsld[["date"]]))) {
+      landsld = landsld[landsld$date_info == "day", ]
+    } else if(class(landsld[["date"]]) == "Date"){
+    cat("Using the column called 'date' to subset the dataframe")
+     landsld = landsld[!is.na(landsld[["date"]]), ]
+    }
+  }else if(any(grepl("Date", sapply(landsld, class)))){
+    cat("Using the first column with class 'Date' to subset the dataframe")
+    cls = sapply(landsld, class)
+    col_idx = grep("Date", cls)[[1]]
+    col = landsld[,col_idx] %>% st_drop_geometry()
+    bin = !is.na(col)
+    landsld = landsld[bin, ]
   }
 
   ####### add an id to the iffi-data
-  iffi10[["iffi_id"]] = 1:nrow(iffi10)
+  landsld[["landsld_id"]] = 1:nrow(landsld)
   poly["poly_id"] = 1:nrow(poly)
 
   ####### if crs are not equal, reproject everything to 32632
-  iffi10 = st_transform(iffi10, epsg)
+  landsld = st_transform(landsld, epsg)
   poly = st_transform(poly, epsg)
 
   ####### crop the poly to the bounding box of the iffi10-points
-  bb = st_bbox(iffi10) %>% st_as_sfc() %>% st_as_sf()
+  bb = st_bbox(landsld) %>% st_as_sfc() %>% st_as_sf()
   poly = st_crop(poly, bb)
 
   ####### for each polygon check which iffi points are in it
-  joined_points_on_poly = sf::st_join(poly, iffi10)
+  joined_points_on_poly = sf::st_join(poly, landsld)
 
   ####### create the output dataframe
   unique_dates_slides_poly = dates_of_slides_in_poly(joined_points_on_poly)
